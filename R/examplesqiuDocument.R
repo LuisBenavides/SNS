a5fcbe102a955937ad044db27c71bc9993fd3702
Luis Benavides <luisbv@tec.mx>
2019-04-08 19:06:49 -0500

    Create examplesqiuDocument.R
    R script to document datasets from Qiu (2014)

---
+++ R/examplesqiuDocument.R
@@ -0,0 +1,163 @@
+#' Data from Example 8.2 Qiu (2014).
+#'
+#' A dataset containing the data set used in Example 8.2 on page 323 of Qiu (2014).
+#'
+#' @format A data frame with 30 rows (batches) and 6 columns
+#' \describe{
+#'  \item{X}{first observation of the batch}
+#'  \item{X.1}{second observation of the batch}
+#'  \item{X.2}{third observation of the batch}
+#'  \item{X.3}{fourth observation of the batch}
+#'  \item{X.4}{fifth observation of the batch}
+#'  \item{Xmedian}{Median of the batch}
+#' }
+#'
+#' (first 5 are the observations, the last is the median of the 5 observations).
+#' The column names are the default names given when the dataframe was created.
+#' @source \url{http://users.phhp.ufl.edu/pqiu/research/book/spc/data/example82.dat}
+"example82"
+
+#' Data from Example 8.4 Qiu (2014).
+#'
+#' A dataset containing the data set used in Example 8.4 of Qiu (2014).
+#'
+#' @format A data frame with 30 rows (batches) and 7 columns:
+#' \describe{
+#'  \item{X}{first observation of the batch}
+#'  \item{X.1}{second observation of the batch}
+#'  \item{X.2}{third observation of the batch}
+#'  \item{X.3}{fourth observation of the batch}
+#'  \item{X.4}{fifth observation of the batch}
+#'  \item{Wn}{Man Withney Statistic}
+#'  \item{Cnn}{Cusum negative statistic}
+#'  \item{Cnp}{Cusum positive statistic}
+#' }
+#' @source \url{http://users.phhp.ufl.edu/pqiu/research/book/spc/data/example84.dat}
+"example84"
+
+#' Data from Example 8.7 on page 339 Qiu (2014).
+#'
+#' A dataset containing the data set used in Example 8.7 on page 339 of Qiu (2014).
+#'
+#' @format A data frame with 100 rows and 2 columns:
+#' \describe{
+#'  \item{x}{first 14 observations are in control}
+#'  \item{Tmax}{test statistic for the method proposed by Hawkins and Deng (2010)}
+#' }
+#' @source \url{http://users.phhp.ufl.edu/pqiu/research/book/spc/data/example87.dat}
+"example87"
+
+#' Data from Example 6.5 on page 246 Qiu (2014).
+#'
+#' A dataset containing the data set used in Example 6.5 on page 246 of Qiu (2014).
+#'
+#' @format A data frame with 30 rows and 5 columns:
+#' \describe{
+#'  \item{x}{first 9 observations are the reference sample. Batch size equals to 1.}
+#'  \item{Wn}{Wn}
+#'  \item{Sn2}{Sn2}
+#'  \item{Bmax}{Bmax}
+#'  \item{hn}{hn}
+#' }
+#' @source \url{http://users.phhp.ufl.edu/pqiu/research/book/spc/data/example65.dat}
+"example65"
+
+#' Data from Example 4.9 Qiu (2014).
+#'
+#' A dataset containing the data set used in Example 4.9 of Qiu (2014).
+#'
+#' @format A data frame with 100 rows and 2 columns:
+#' \describe{
+#'  \item{V1}{First data set with 100 observations divided in 20 batches of 5 observation each. The first 10 batches are N(0,1) and the second 10 batches are N(1,1).}
+#'  \item{V2}{Second data set with 100 observations divided in 20 batches of 5 observation each. The first 10 batches are N(0,1) and the second 10 batches are N(0,22).}
+#' }
+#' @source \url{http://users.phhp.ufl.edu/pqiu/research/book/spc/data/example49.dat}
+"example49"
+
+#' Data from Example 8.1 on page 319 Qiu (2014).
+#'
+#' A dataset containing the data set used in Example 8.1 on page 319 of Qiu (2014).
+#'
+#' @format A data frame with 30 rows and 11 columns. Each row is a batch. The first 10 columns are the observation of each batch:
+#' \describe{
+#'  \item{X}{1st observation of the batch.}
+#'  \item{X.1}{2nd observation of the batch.}
+#'  \item{X.2}{3rd observation of the batch.}
+#'  \item{X.3}{4th observation of the batch.}
+#'  \item{X.4}{5th observation of the batch.}
+#'  \item{X.5}{6th observation of the batch.}
+#'  \item{X.6}{7th observation of the batch.}
+#'  \item{X.7}{8th observation of the batch.}
+#'  \item{X.8}{9th observation of the batch.}
+#'  \item{X.9}{10th observation of the batch.}
+#'  \item{psi}{psi}
+#' }
+#' @source \url{http://users.phhp.ufl.edu/pqiu/research/book/spc/data/example81.dat}
+"example81"
+
+#It consists of 30 batches of observations where each batch has 6 observations
+
+#' Data from Example 8.3 on page 326 Qiu (2014).
+#'
+#' A dataset containing the data set used in Example 8.3 on page 326 of Qiu (2014).
+#'
+#' @format A data frame with 30 rows and 8 columns. Each row is a batch. The first 6 columns are the observation of each batch:
+#' \describe{
+#'  \item{X}{1st observation of the batch.}
+#'  \item{X.1}{2nd observation of the batch.}
+#'  \item{X.2}{3rd observation of the batch.}
+#'  \item{X.3}{4th observation of the batch.}
+#'  \item{X.4}{5th observation of the batch.}
+#'  \item{X.5}{6th observation of the batch.}
+#'  \item{psi}{psi}
+#' }
+#' @source \url{http://users.phhp.ufl.edu/pqiu/research/book/spc/data/example81.dat}
+"example83"
+
+"example85"
+
+#' Data from Example 9.1 on page 369 Qiu (2014).
+#'
+#' A dataset containing the data set used in Example 9.1 on page 369 of Qiu (2014).
+#'
+#' @format The data (X,Y) consist of 20 batches with 50 observations in each batch.
+#'
+#' \describe{
+#'  \item{V1}{1st batch.}
+#'  \item{V2}{2nd batch.}
+#' }
+#' @source \url{http://users.phhp.ufl.edu/pqiu/research/book/spc/data/example91.dat}
+"example91"
+
+#' Data from Example 9.3 Qiu (2014).
+#'
+#' A dataset containing the data set used in Example 9.3 of Qiu (2014).
+#'
+#' @format The data (X,Y) consist of 20 batches with 10 observations in each batch.
+#'
+#' \describe{
+#'  \item{X}{1st batch.}
+#'  \item{X.1}{2nd batch.}
+#' }
+#' @source \url{http://users.phhp.ufl.edu/pqiu/research/book/spc/data/example93.dat}
+"example93"
+
+#' Data from Example 7.1 Qiu (2014).
+#'
+#' A dataset containing the data set used in Example 7.1 of Qiu (2014).
+#'
+#' @format The data (X1,X2,X3) consist of 30 observations each variable.
+#'
+#' \describe{
+#'  \item{X}{1st batch.}
+#'  \item{X.1}{2nd batch.}
+#'  \item{X.2}{3rd batch.}
+#' }
+#' @source \url{http://users.phhp.ufl.edu/pqiu/research/book/spc/data/example71.dat}
+"example71"
+
+"example74a"
+
+"example74b"
+
+"example74c"
