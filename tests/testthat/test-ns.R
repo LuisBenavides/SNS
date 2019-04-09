d1a08accca73f791985fcb5820ff38e1b804b1bb
Luis Benavides <luisbv@tec.mx>
2019-04-08 19:11:41 -0500

    comment tests
    In the next update the tests are going to be incorporated

--- tests/testthat/test-ns.R
+++ tests/testthat/test-ns.R
@@ -1,5 +1,21 @@
-context("test-ns")
+library("SNS")
+context("test-sns")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("normal score and rank", {
+  Y <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+  X <- c(30, 35, 45)
+  theta <- 40
+  Ftheta <- 0.5
+  # Test conditional
+  ns <- NS(X = X, Y = Y, theta = theta, Ftheta = Ftheta)
+  #expect_equal(ns$Z, c(-0.52440051,-0.31863936,0.08964235), tolerance=1e-7)
+  #expect_equal(ns$R, c(3.5,4,1), tolerance=1e-10)
+
+  theta <- NULL
+  Ftheta <- NULL
+  # Test unconditional
+  ns <- NS(X = X, Y = Y, theta = theta, Ftheta = Ftheta)
+  #expect_equal(ns$Z, c(-0.6045853,-0.4727891,-0.2298841), tolerance=1e-7)
+  #expect_equal(ns$R, c(3.5,4,5), tolerance=1e-10)
+
 })
--- tests/testthat/test-sns.R
+++ tests/testthat/test-sns.R
@@ -1,5 +1,31 @@
+library("SNS")
+
 context("test-sns")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("sequential normal scores", {
+  Y <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+  X <- c(30, 35, 45)
+  theta <- 40
+  Ftheta <- 0.5
+  sample.id <- c("a", "b", "c")
+  # Test CONDITIONAL WITH REFERENCE SAMPLE
+  #expect_equal(SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta), c(-0.52440051,-0.38532047,0.08964235), tolerance=1e-7)
+
+
+  # Test UNCONDITIONAL WITH REFERENCE SAMPLE
+  theta <- NULL
+  Ftheta <- NULL
+  #expect_equal(SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta), c(-0.6045853,-0.3186394,0.0000000), tolerance=1e-7)
+
+  # EXAMPLE CONDITIONAL WITHOUT REFERENCE SAMPLE
+  Y <- NULL
+  theta <- 40
+  Ftheta <- 0.5
+  #expect_equal(SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta), c(-0.6744898,-0.3186394,0.6744898), tolerance=1e-7)
+
+  # EXAMPLE UNCONDITIONAL WITHOUT REFERENCE SAMPLE
+  theta <- NULL
+  Ftheta <- NULL
+
+  #expect_equal(SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta), c(0.0000000,0.6744898,0.9674216), tolerance=1e-7)
 })
