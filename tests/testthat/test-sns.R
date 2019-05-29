b3d3a029da9eb1ed1c8e554266a0f39ed58de171
Luis Benavides <luisbv@tec.mx>
2019-05-29 15:33:44 -0500

    update tests

--- tests/testthat/test-sns.R
+++ tests/testthat/test-sns.R
@@ -9,23 +9,30 @@ test_that("sequential normal scores", {
   Ftheta <- 0.5
   sample.id <- c("a", "b", "c")
   # Test CONDITIONAL WITH REFERENCE SAMPLE
-  #expect_equal(SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta), c(-0.52440051,-0.38532047,0.08964235), tolerance=1e-7)
+  output <- SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta)
+  expectedOutput <- c(-0.52440051,-0.31863936,0.08964235)
+  expect_equal(output$Z, expectedOutput, tolerance=1e-7)
 
 
   # Test UNCONDITIONAL WITH REFERENCE SAMPLE
   theta <- NULL
   Ftheta <- NULL
-  #expect_equal(SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta), c(-0.6045853,-0.3186394,0.0000000), tolerance=1e-7)
+  output <- SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta)
+  expectedOutput <- c(-0.6045853,-0.3186394,0.0000000)
+  expect_equal(output$Z, expectedOutput, tolerance=1e-7)
 
   # EXAMPLE CONDITIONAL WITHOUT REFERENCE SAMPLE
   Y <- NULL
   theta <- 40
   Ftheta <- 0.5
-  #expect_equal(SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta), c(-0.6744898,-0.3186394,0.6744898), tolerance=1e-7)
+  output <- SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta)
+  expectedOutput <- c(-0.6744898,-0.2104284,0.6744898)
+  expect_equal(output$Z, expectedOutput, tolerance=1e-7)
 
   # EXAMPLE UNCONDITIONAL WITHOUT REFERENCE SAMPLE
   theta <- NULL
   Ftheta <- NULL
-
-  #expect_equal(SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta), c(0.0000000,0.6744898,0.9674216), tolerance=1e-7)
+  output <- SNS(X = X, X.id = sample.id, Y = Y, theta = theta, Ftheta = Ftheta)
+  expectedOutput <- c(0.0000000,0.9674216,1.1503494)
+  expect_equal(output$Z, expectedOutput, tolerance=1e-7)
 })
